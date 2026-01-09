9bae2492c12b64a97190fe3f8bd4ef98ae669ea3
luca_calamo <lucacalamo13@gmail.com>
2026-01-09 13:45:06 -0800

    changed image and centered

--- .history/app/page_20260109134215.tsx
+++ .history/app/page_20260109134215.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className='container'>
-            <div className='content'>
+            <div>
                 <h1>Under Construction!</h1>
                 <img
                     src='https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGRteDZrc3lqcWN5YXFiM2U0dXR2ZG9tdzFqcGd3ODR5cm5mZ2xnNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mcsPU3SkKrYDdW3aAU/giphy.gif'
