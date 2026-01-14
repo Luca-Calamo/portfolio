1dfac7ae604ce9f4225e7390c86971ee5e631e54
luca_calamo <lucacalamo13@gmail.com>
2026-01-13 20:55:32 -0800

    styled select your project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,6 +7,8 @@ const projects = [
     { id: 2, name: 'Project 2' },
     { id: 3, name: 'Project 3' },
     { id: 4, name: 'Project 4' },
+    { id: 5, name: 'Project 5' },
+    { id: 6, name: 'Project 6' },
 ];
 
 export default function Projects() {
